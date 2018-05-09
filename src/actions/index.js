5988f9224e05cb11561fe11a87b47323eadbb091
Duy Nguyen <duyng93@gmail.com>
2018-05-09 15:34:56 -0700

    completed getting data from server

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
 import types from './types';
+import axios from 'axios';
+
+const BASE_URL = 'http://api.reactprototypes.com';
+const API_KEY = '?key=somethinghasntbeenusedbefore';
+
+export function getList(){
+    const response = axios.get(`${BASE_URL}/TODOS${API_KEY}`);
+
+    return{
+        type: types.GET_LIST_DATA,
+        payload: response
+    }
+}
+
